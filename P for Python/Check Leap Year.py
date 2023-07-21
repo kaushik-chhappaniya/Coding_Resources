94baa310fe96fe48a464f03ee2fb4e6b6e1787ad
Kaushik Kanhaiya Chhappaniya <52828273+kaushik-chhappaniya@users.noreply.github.com>
2023-07-21 15:19:28 +0530

    Update and rename Leap_year_or_not.py to Check Leap Year.py

--- P for Python/Check Leap Year.py
+++ P for Python/Check Leap Year.py
@@ -1,6 +1,9 @@
-#Coding_Resources
-
 #!/usr/bin/env python
+#Coding_Resources
+""" Check if the given year is a leap year or not """
+__author__      = "Kaushik Chhappaniya"
+__copyright__   = "Copyright 2023, Coding_Resources"
+
 print("Enter a year to check if it Leap year or not")
 year = int(input())
 if year%4 == 0:
