d55413fad0bac01b59913cf314b71e1624cb585c
Kaushik Kanhaiya Chhappaniya <52828273+kaushik-chhappaniya@users.noreply.github.com>
2023-07-21 15:14:30 +0530

    Update Check Quadrant Of Two Number.py

--- P for Python/Check Quadrant Of Two Number.py
+++ P for Python/Check Quadrant Of Two Number.py
@@ -1,6 +1,9 @@
-#Coding_Resources
-
 #!/usr/bin/env python
+#Coding_Resources
+""" Check Quadrants based on two numbers """
+__author__      = "Kaushik-Chhappaniya"
+__copyright__   = "Copyright 2023, Coding_Resources"
+
 def check(x,y):
   if x>0 and y>0:
   	print("The point ("+str(x)+","+str(y)+") is in the First Quadrant")
@@ -16,6 +19,7 @@ def check(x,y):
 
 
 
-x = input("Enter the value of X coordinate\n")
-y = input("Enter the value of Y coordinate\n")
-check(x,y)
+if __name__ == "__main__":
+  x = input("Enter the value of X coordinate\n")
+  y = input("Enter the value of Y coordinate\n")
+  check(x,y)
