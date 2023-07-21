ffd022c966c2177a1bdd3cbe3d4db5c5a96d964b
Kaushik Kanhaiya Chhappaniya <52828273+kaushik-chhappaniya@users.noreply.github.com>
2023-07-21 15:16:05 +0530

    Update Check Whether Number is Positive or not.py

--- P for Python/Check Whether Number is Positive or not.py
+++ P for Python/Check Whether Number is Positive or not.py
@@ -1,6 +1,10 @@
-#Coding_Resources  
+#!/usr/bin/env python
+#Coding_Resources
+""" Check if the given number is posiive or negative """
+__author__      = "Kaushik Chhappaniya"
+__copyright__   = "Copyright 2023, Coding_Resources"
 
-#!usr/bin/env python
+
 print("Enter a number to check for positive or not")
 num = int(input())
 if num>0:
@@ -9,3 +13,4 @@ elif num<0:
 	print("The number"+str(num)+ " you have entered is Negative")	#Converting the int data type to string
 else:
 	print("The number you have entered is 0")
+
